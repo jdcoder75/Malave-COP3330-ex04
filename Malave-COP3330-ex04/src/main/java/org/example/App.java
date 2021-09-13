e8d54f06f941b51b725e64c0f4fb88f25920e2c1
jdcoder75 <89999560+jdcoder75@users.noreply.github.com>
2021-09-13 17:43:48 -0400

    Add files via upload

---
+++ Malave-COP3330-ex04/src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Jose Malave
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a noun: " );
+        String noun= sc.nextLine();
+        System.out.println("Enter a verb: " );
+        String verb= sc.nextLine();
+        System.out.println("Enter a adjective: " );
+        String adjective= sc.nextLine();
+        System.out.println("Enter a adverb: " );
+        String adverb= sc.nextLine();
+        System.out.println("Do you " + verb +" your " + adjective + " "+ noun + " "+ adverb + "? That's hilarious!");
+    }
+}
